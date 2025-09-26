f4ce5e97c5f71b20155b300e31666ea474f9ca05
Md Rakibuzzaman Rakib <rakibzaman724@gmail.com>
2025-09-27 00:48:03 +0300

    2.4: Course information step 9

--- part2/course_info/src/App.jsx
+++ part2/course_info/src/App.jsx
@@ -3,7 +3,7 @@ const Header = ({course}) => {
 
   return (
     <div>
-      <h1>{course.name}</h1>
+      <h3>{course.name}</h3>
     </div>
   )
 }
@@ -23,10 +23,13 @@ const Content = ({course})=> {
 
   return (
     <div>
-      <Part parts={course.parts[0]}/>
-      <Part parts={course.parts[1]}/>
-      <Part parts={course.parts[2]}/>
-      <Part parts={course.parts[3]}/>
+      {course.parts.map(p => {
+        console.log('After mapping course contents:', p);
+        
+        return(
+          <Part key={p.id} parts={p} />
+        )
+      })}
     </div>
     )
 }
@@ -55,35 +58,62 @@ const Course = ({course}) => {
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+console.log('App rendering "courses"', courses)
 
   return (
     <div>
-      <Course course={course} />
+      <h2>Web development curriculum</h2>
+      {courses.map(c => {
+        console.log('after mapping course:', c)
+
+        return(
+          <Course key={c.id} course={c} />
+        )
+      })}
     </div>
   )
 }
